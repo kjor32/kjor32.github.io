4fffe28b520a79274a2dda63dd6e7492d5b0a345
kjor32 <100085444+kjor32@users.noreply.github.com>
2023-05-02 20:30:28 +0300

    Initial commit

--- js/main.js
+++ js/main.js
@@ -2,13 +2,20 @@ const showProjectsBtn = document.getElementById('show-projects-btn');
 const projectsContainer = document.getElementById('projects-container');
 
 showProjectsBtn.addEventListener('click', () => {
-  projectsContainer.style.display = 'block'; // показываем контейнер с проектами
+  if (projectsContainer.style.display === 'none') {
+    projectsContainer.style.display = 'block'; 
+  } else {
+    projectsContainer.style.display = 'none'; 
+  }
 });
 
 const showContactsBtn = document.getElementById('show-contacts-btn');
 const contactsContainer = document.getElementById('contacts-container');
 
 showContactsBtn.addEventListener('click', () => {
-  contactsContainer.style.display = 'block'; // показываем контейнер с контактами
+  if (contactsContainer.style.display === 'none') {
+    contactsContainer.style.display = 'block'; 
+  } else {
+    contactsContainer.style.display = 'none'; 
+  }
 });
-
