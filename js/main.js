34782ba8ef27966f5904a19bcf14d8dd1d67faad
kjor32 <100085444+kjor32@users.noreply.github.com>
2023-05-06 18:46:01 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,24 +1,13 @@
 const showProjectsBtn = document.getElementById('show-projects-btn');
 const projectsContainer = document.getElementById('projects-container');
 
-showProjectsBtn.addEventListener('click', () => {
-  if (projectsContainer.style.display === 'none') {
-    projectsContainer.style.display = 'block'; 
-  } else {
-    projectsContainer.style.display = 'none'; 
-  }
-});
-
 const showContactsBtn = document.getElementById('show-contacts-btn');
 const contactsContainer = document.getElementById('contacts-container');
 
-showContactsBtn.addEventListener('click', () => {
-  if (contactsContainer.style.display === 'none') {
-    contactsContainer.style.display = 'block'; 
-  } else {
-    contactsContainer.style.display = 'none'; 
-  }
+showProjectsBtn.addEventListener('click', function(){
+    projectsContainer.style.display ? projectsContainer.style.display = 'none' : projectsContainer.style.display = 'block';
 });
 
-
-
+showContactsBtn.addEventListener('click', function(){
+    contactsContainer.style.display ? contactsContainer.style.display = 'none' : contactsContainer.style.display = 'block';
+});
